fbd49552a5d6e803a5fa9bddbe5fa617cf0923db
Pilar Romero <pilar07@MacBook-Pro-de-Pilar07.local>
2019-02-22 15:39:58 -0500

    first commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var numberFirst:Double = 0
+    var numberSecond:Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    enum Calculator:String{
+        case suma = "+"
+        case resta = "-"
+        case division = "\\"
+        case multiplicacion = "*"
+        
+        func operacion(numA:Int, numB:Int) -> Double {
+            switch self {
+            case .suma:
+                return Double(numA + numB)
+            case .resta:
+                return Double(numA - numB)
+            case .division:
+                return Double(numA / numB)
+            case .multiplicacion:
+                return Double(numA * numB)
+            }
+        }
+    }
+    
+    @IBAction func numberButton(_ sender: UIButton) {
+        resultLabel.text = "\(resultLabel.text!)" + "\(sender.titleLabel?.text! ?? "")"
+    
+    }
 
 }
 
---
+++ core/core.h
@@ -0,0 +1,19 @@
+//
+//  core.h
+//  core
+//
+//  Created by Pilar07 on 1/31/19.
+//  Copyright © 2019 Done Task. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for core.
+FOUNDATION_EXPORT double coreVersionNumber;
+
+//! Project version string for core.
+FOUNDATION_EXPORT const unsigned char coreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <core/PublicHeader.h>
+
+
